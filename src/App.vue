8d3b3739e97e36596efe418a31c709afc0ded494
m.fallah <m.fallah@kias.ir>
2022-12-26 10:33:47 +0330

    spell checher

--- src/App.vue
+++ src/App.vue
@@ -5,30 +5,30 @@ import { remove } from '@vue/shared';
 export default {
   data() {
     return {
-      key_change:-1,
-      edit_mode : false,
+      key_change: -1,
+      edit_mode: false,
       content: '',
       items: []
     }
   },
   methods: {
     add_to_list(text) {
-      if(!this.items.includes(text)){
+      if (!this.items.includes(text)) {
         this.items.push(text);
         this.content = '';
       }
     },
     remove(value) {
-      this.items.splice(this.items.indexOf(value), 1);  
+      this.items.splice(this.items.indexOf(value), 1);
     },
-    set_value_input(value){
+    set_value_input(value) {
       this.content = this.items[value];
-      this.edit_mode =true;
+      this.edit_mode = true;
       this.key_change = value;
     },
-    applay_change(new_value){
+    apply_change(new_value) {
       this.items[this.key_change] = new_value;
-      this.edit_mode =false;
+      this.edit_mode = false;
       this.content = '';
     }
   }
@@ -38,7 +38,7 @@ export default {
 <template>
   <input v-model="content">
   <button v-if="!edit_mode" @click="add_to_list(content)">add</button>
-  <button v-else @click="applay_change(content)">Apply</button>
+  <button v-else @click="apply_change(content)">Apply</button>
   <li v-for="(item, key) in items">
     {{ item }} <button @click="remove(item)">remove</button>
     <button @click="set_value_input(key)">Edit</button>
