4a94a08af8f382b10a8e9683e4529d685e207cfb
m.fallah <m.fallah@kias.ir>
2022-12-26 10:30:41 +0330

    added update value

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ import { remove } from '@vue/shared';
 export default {
   data() {
     return {
+      key_change:-1,
+      edit_mode : false,
       content: '',
       items: []
     }
@@ -18,6 +20,16 @@ export default {
     },
     remove(value) {
       this.items.splice(this.items.indexOf(value), 1);  
+    },
+    set_value_input(value){
+      this.content = this.items[value];
+      this.edit_mode =true;
+      this.key_change = value;
+    },
+    applay_change(new_value){
+      this.items[this.key_change] = new_value;
+      this.edit_mode =false;
+      this.content = '';
     }
   }
 }
@@ -25,8 +37,10 @@ export default {
 
 <template>
   <input v-model="content">
-  <button @click="add_to_list(content)">add</button>
-  <li v-for="item in items">
+  <button v-if="!edit_mode" @click="add_to_list(content)">add</button>
+  <button v-else @click="applay_change(content)">Apply</button>
+  <li v-for="(item, key) in items">
     {{ item }} <button @click="remove(item)">remove</button>
+    <button @click="set_value_input(key)">Edit</button>
   </li>
 </template>
