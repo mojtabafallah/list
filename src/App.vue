b1ffb41393d69a5c8902141f3d9bb99b38605dd0
m.fallah <m.fallah@kias.ir>
2022-12-26 10:13:37 +0330

    list unique

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script>
-  
+import { remove } from '@vue/shared';
+
+
+export default {
+  data() {
+    return {
+      content: '',
+      items: []
+    }
+  },
+  methods: {
+    add_to_list(text) {
+      if(!this.items.includes(text)){
+        this.items.push(text);
+        this.content = '';
+      }
+    },
+    remove(value) {
+      this.items.splice(this.items.indexOf(value), 1);  
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <p>test</p>
-  </div>
+  <input v-model="content">
+  <button @click="add_to_list(content)">add</button>
+  <li v-for="item in items">
+    {{ item }} <button @click="remove(item)">remove</button>
+  </li>
 </template>
-
