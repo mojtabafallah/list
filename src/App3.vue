bac0e27333391673c4479a83ae58407aa1e92bc3
unknown <mojtaba.fallah1373@gmail.com>
2022-12-29 17:58:40 +0330

    formated

--- src/App3.vue
+++ src/App3.vue
@@ -1,77 +1,76 @@
 <script>
- export default{
-   data(){
-    return{
-       color: 'red',
-       fs: 20,
-       users: [
-        {
-            id: 1,
-            name:'ali',
-            family: 'alavi',
-            age: 30,
-            job: 'developer'
-        },
-        {
-            id: 2,
-            name:'hasan',
-            family: 'alavi',
-            age: 35,
-            job: 'developer2'
-        },
-        {
-            id: 3,
-            name:'reza',
-            family: 'rezaee',
-            age: 36,
-            job: 'developer3'
-        },
-        {
-            id: 1,
-            name:'ali',
-            family: 'alavi',
-            age: 30,
-            job: 'developer4'
-        }
-       ],
-       user_data: {
+export default {
+    data() {
+        return {
+            color: 'red',
+            fs: 20,
+            users: [
+                {
+                    id: 1,
+                    name: 'ali',
+                    family: 'alavi',
+                    age: 30,
+                    job: 'developer'
+                },
+                {
+                    id: 2,
+                    name: 'hasan',
+                    family: 'alavi',
+                    age: 35,
+                    job: 'developer2'
+                },
+                {
+                    id: 3,
+                    name: 'reza',
+                    family: 'rezaee',
+                    age: 36,
+                    job: 'developer3'
+                },
+                {
+                    id: 1,
+                    name: 'ali',
+                    family: 'alavi',
+                    age: 30,
+                    job: 'developer4'
+                }
+            ],
+            user_data: {
 
-       }
+            }
+        }
+    },
+    computed: {
+        checkAge() {
+            return this.users.filter(item => item.age > 32);
+        }
     }
-   },
-   computed:{
-    checkAge()
-    {
-        return this.users.filter(item => item.age > 32);         
-    }
-   }
-   
- }
+
+}
 </script>
 
 <template>
-<p :style="{color: color, fontSize: fs + 'px'}"> this is a text </p>
-<table border="1" cellpadding="0" cellspacing="0">
-    <thead>
-        <th>row</th>
-        <th>name family</th>
-        <th>age</th>
-        <th>job</th>
-    </thead>
-    <tbody>
-        <tr v-for="(user, index) in users" :key="user.id" >
-            <td>{{ index + 1 }}</td>
-            <td>{{ user.name + user.family }}</td>
-            <td>{{ user.age }}</td>
-            <td>{{ user.job }}</td>
-        </tr>
-    </tbody>
-</table>
-<ul>
+    <p :style="{ color: color, fontSize: fs + 'px' }"> this is a text </p>
+    <table border="1" cellpadding="0" cellspacing="0">
+        <thead>
+            <th>row</th>
+            <th>name family</th>
+            <th>age</th>
+            <th>job</th>
+        </thead>
+        <tbody>
+            <tr v-for="(user, index) in users" :key="user.id">
+                <td>{{ index + 1 }}</td>
+                <td>{{ user.name + user.family }}</td>
+                <td>{{ user.age }}</td>
+                <td>{{ user.job }}</td>
+            </tr>
+        </tbody>
+    </table>
+    <ul>
 
-    <li v-for="user in checkAge">{{ user.name +  '-' + user.age }}</li>
+        <li v-for="user in checkAge">{{ user.name + '-' + user.age }}</li>
 
-</ul>
+    </ul>
 </template>
 <style>
 
