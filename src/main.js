d42cd40f8940f01d5854f49278cd50245b53c6d6
unknown <mojtaba.fallah1373@gmail.com>
2022-12-29 17:48:59 +0330

    computed learn

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import App2 from './App2.vue'
+import App3 from './App3.vue'
 
 
 // createApp(App).mount('#app')
-createApp(App2).mount('#app')
+// createApp(App2).mount('#app')
+createApp(App3).mount('#app')
